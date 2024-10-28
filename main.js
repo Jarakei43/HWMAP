2a111e71c5661cc880ef1816802463a2949ff90a
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2024-10-28 18:17:48 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// N1
+// const num =  [1, 2, 3]
+// const fraction = num.map((item) => {
+//     return item *2
+// })
+// console.log(fraction);
+
+// N2
+// const str =  ['привет', 'мир']
+// const upper = str.map((item) => {
+//     return item.toUpperCase()
+// })
+// console.log(upper);
+
+// N3
+// const str2 =  [1, 2, 3, 4]
+// const toStr = str2.map((item) => {
+//     return String(item)
+// })
+// console.log(toStr);
+
+// N4
+// const animals = ['кот', 'собака', 'тигр']
+// const animalsMap = animals.map((item) =>{
+//     return item + "!"
+// })
+// console.log(animalsMap);
+
+// N5
+// const users = [
+//   { name: "Alice", age: 47 },
+//   { name: "Hannah", age: 30 },
+//   { name: "Fiona", age: 24 },
+//   { name: "George", age: 35 },
+//   { name: "Bob", age: 50 },
+//   { name: "Alice", age: 31 },
+//   { name: "Charlie", age: 52 },
+//   { name: "George", age: 31 },
+//   { name: "Hannah", age: 21 },
+//   { name: "George", age: 35 },
+// ];
+// const names = users.map((item) => {
+//   return item.name;
+// });
+// console.log(names);
